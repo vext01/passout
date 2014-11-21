4a18871c087e29fa68fc267523d36c2e0c218191
Edd Barrett <vext01@gmail.com>
2014-11-21 16:52:29 +0000

    Add some suggestions for tests.

--- tests/test_cmds.py
+++ tests/test_cmds.py
@@ -14,3 +14,18 @@ class TestCmds(support.PexpectTest):
         child2 = self.run_passout("stdout", rand_pwname)
         child2.expect(rand_pw)
         child2.expect(pexpect.EOF)
+
+    # XXX Test printconfig
+
+    # XXX Test 'ls'
+
+    # XXX add a password, delete a password,
+    # try to print a password, should fail
+
+    # XXX Test xclip route.
+
+    # XXX add a password, add a password of the same name, should fail.
+
+    # XXX delete a password without adding one, should fail.
+
+    # XXX Try to add a password under a non-existent GPG id. Should fail.
