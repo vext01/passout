882766a18f5783c4b151ac35f934d288e89f41a3
Edd Barrett <vext01@gmail.com>
2015-07-01 22:00:21 +0100

    Hook tests into setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
 
 long_description = open("README.md", "r").read()
 
@@ -8,6 +9,25 @@ requirements = [
     "pytest",
     "sh",
 ]
+
+class PyTest(TestCommand):
+
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        # import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
 
 setup(
     name="passout",
@@ -31,4 +51,6 @@ setup(
     ],
     keywords="password manager gpg",
     scripts=["passout_cli"],
+    tests_require=['pytest'],
+    cmdclass={'test': PyTest},
 )
