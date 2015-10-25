11c2a807b1825cbbbb869e2ed1b74a9cefb8f7ed
Edd Barrett <vext01@gmail.com>
2015-10-25 16:21:46 +0000

    Remove test target from setup.py.
    This has been pretty unreliable for us in the past. Use pytest in the
    source directory instead.

--- setup.py
+++ setup.py
@@ -1,6 +1,4 @@
-import sys
 from setuptools import setup, find_packages
-from setuptools.command.test import test as TestCommand
 from passout import VERSION
 
 LONG_DESCRIPTION = open("README.md", "r").read()
@@ -12,25 +10,6 @@ REQUIREMENTS = [
     "sh",
 ]
 
-
-class PyTest(TestCommand):
-
-    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
-
-    def initialize_options(self):
-        TestCommand.initialize_options(self)
-        self.pytest_args = []
-
-    def finalize_options(self):
-        TestCommand.finalize_options(self)
-        self.test_args = []
-        self.test_suite = True
-
-    def run_tests(self):
-        # import here, cause outside the eggs aren't loaded
-        import pytest
-        errno = pytest.main(self.pytest_args)
-        sys.exit(errno)
 
 setup(
     name="passout",
@@ -59,5 +38,4 @@ setup(
         ]
     },
     tests_require=['pytest'],
-    cmdclass={'test': PyTest},
 )
